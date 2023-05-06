86878f04c399cf40d1b592609f601c7450c6c7a8
subhibhat <s_subhibhat@hotmail.com>
2023-05-06 13:55:42 +0700

    update footer style

--- components/layouts/Footer.vue
+++ components/layouts/Footer.vue
@@ -1,21 +1,26 @@
 <template>
-	<footer class="bg-dark text-light py-4">
-		<b-container>
-			<b-row>
-				<b-col cols="12" lg="6" md="6">
-					<p class="text-light small pb-0 mb-0">
-					<ol class="p-0 m-0" style="list-style: none;">
-						<li>ระบบทุนการศึกษาออนไลน์</li>
-						<li>งานพัฒนานักศึกษา คณะวิศวกรรมศาสตร์ มหาวิทยาลัยอุบลราชธานี</li>
-						<li>85 ถ.สถลมาร์ค ต.เมืองศรีไค อ.วารินชำราบ จ.อุบลราชธานี 34190</li>
-					</ol>
-					</p>
-				</b-col>
-				<b-col cols="12" lg="6" md="6" class="text-left text-md-right">
-					<nuxt-link to="/policy">นโยบายคุกกี้</nuxt-link>
-				</b-col>
-			</b-row>
-		</b-container>
-	</footer>
+  <footer class="py-4">
+    <b-container>
+      <b-row>
+        <b-col>
+          <hr class="mt-0" />
+        </b-col>
+      </b-row>
+      <b-row>
+        <b-col cols="12" lg="6" md="6">
+          <p class="text-muted small pb-0 mb-0">
+          <ol class="p-0 m-0" style="list-style: none;">
+            <li>ระบบทุนการศึกษาออนไลน์</li>
+            <li>งานพัฒนานักศึกษา คณะวิศวกรรมศาสตร์ มหาวิทยาลัยอุบลราชธานี</li>
+            <li>85 ถ.สถลมาร์ค ต.เมืองศรีไค อ.วารินชำราบ จ.อุบลราชธานี 34190</li>
+          </ol>
+          </p>
+        </b-col>
+        <b-col cols="12" lg="6" md="6" class="text-left text-md-right">
+          <nuxt-link to="/policy" class="text-muted">นโยบายคุกกี้</nuxt-link>
+        </b-col>
+      </b-row>
+    </b-container>
+  </footer>
 </template>
 
