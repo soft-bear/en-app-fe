50f981b5c735c3ae37384577f99cfba0e4593934
subhibhat <s_subhibhat@hotmail.com>
2023-05-06 14:23:49 +0700

    set app_title and example .env

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,69 +1,69 @@
 export default {
-    // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
-    ssr: false,
+  // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
+  ssr: false,
 
-    // Target: https://go.nuxtjs.dev/config-target
-    target: 'static',
+  // Target: https://go.nuxtjs.dev/config-target
+  target: 'static',
 
-    // Global page headers: https://go.nuxtjs.dev/config-head
-    head: {
-        title: 'en-ubu-fe',
-        htmlAttrs: {
-            lang: 'en'
-        },
-        meta: [
-            { charset: 'utf-8' },
-            { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-            { hid: 'description', name: 'description', content: '' },
-            { name: 'format-detection', content: 'telephone=no' }
-        ],
-        link: [
-            { rel: 'icon', type: 'image/x-icon', href: '/en-ubu-logo.ico' }
-        ]
+  // Global page headers: https://go.nuxtjs.dev/config-head
+  head: {
+    title: process.env.APP_TITLE || 'en-ubu-fe',
+    htmlAttrs: {
+      lang: 'en'
     },
-
-    // Global CSS: https://go.nuxtjs.dev/config-css
-    css: [
-        '@/assets/css/app.css',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' },
+      { name: 'format-detection', content: 'telephone=no' }
     ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/en-ubu-logo.ico' }
+    ]
+  },
 
-    // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-    plugins: [],
+  // Global CSS: https://go.nuxtjs.dev/config-css
+  css: [
+    '@/assets/css/app.css',
+  ],
 
-    // Auto import components: https://go.nuxtjs.dev/config-components
-    components: true,
+  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  plugins: [],
 
-    // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
-    buildModules: [
-        '@nuxtjs/fontawesome'
-    ],
+  // Auto import components: https://go.nuxtjs.dev/config-components
+  components: true,
 
-    fontawesome: {
-        icons: {
-            solid: [
-                'faHouse',
-                'faRightToBracket',
-                'faUserPlus'
-            ],
-        }
-    },
+  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  buildModules: [
+    '@nuxtjs/fontawesome'
+  ],
 
-    // Modules: https://go.nuxtjs.dev/config-modules
-    modules: [
-        // https://go.nuxtjs.dev/bootstrap
-        'bootstrap-vue/nuxt',
-        // https://go.nuxtjs.dev/axios
-        '@nuxtjs/axios',
-    ],
+  fontawesome: {
+    icons: {
+      solid: [
+        'faHouse',
+        'faRightToBracket',
+        'faUserPlus'
+      ],
+    }
+  },
 
-    // Axios module configuration: https://go.nuxtjs.dev/config-axios
-    axios: {
-        // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-        baseURL: process.env.API_URL || 'https://en-ubu-be.test',
-    },
+  // Modules: https://go.nuxtjs.dev/config-modules
+  modules: [
+    // https://go.nuxtjs.dev/bootstrap
+    'bootstrap-vue/nuxt',
+    // https://go.nuxtjs.dev/axios
+    '@nuxtjs/axios',
+  ],
 
-    // Build Configuration: https://go.nuxtjs.dev/config-build
-    build: {}
+  // Axios module configuration: https://go.nuxtjs.dev/config-axios
+  axios: {
+    // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
+    baseURL: process.env.API_URL || 'https://en-ubu-be.test',
+  },
+
+  // Build Configuration: https://go.nuxtjs.dev/config-build
+  build: {}
 }
 
 //         // this `extend` callback is only needed if using Yarn Plug-n-Play
