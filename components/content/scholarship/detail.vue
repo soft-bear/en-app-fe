510b1b484d7e670a6dd6e7dfe0a56d859fca83f4
subhibhat <s_subhibhat@hotmail.com>
2023-05-27 11:46:03 +0700

    update view

--- components/content/scholarship/detail.vue
+++ components/content/scholarship/detail.vue
@@ -1,44 +1,114 @@
 <template>
   <b-container>
     <b-overlay :show="loading">
-      <b-row class="my-4">
+      <div class="row my-4">
         <b-col>
-          <b-card>
-            <template #header>
-              <b-row>
-                <b-col>{{ data.name }}</b-col>
-                <b-col class="text-right small">{{ formatdate(data.created_at) }} | โดย : {{ data?.author?.username }}</b-col>
-              </b-row>
-            </template>
-            <b-row v-if="data.content">
-              <b-col class="card-post-detail" v-html="data.content"></b-col>
-            </b-row>
-            <b-row v-if="data.content">
-              <b-col>
-                <hr class="my-2" />
-              </b-col>
-            </b-row>
-            <b-row>
-              <b-col>
-                <b-card header-tag="div" class="small" v-if="data.document">
-                  <a :href="data.document.path" target="_blank" noopener noreferrer>{{ data.document.name }}</a>
-                </b-card>
-                <b-card header-tag="div" class="small" v-else>
-                  ไม่มีเอกสารประกอบ
-                </b-card>
-              </b-col>
-            </b-row>
-          </b-card>
+          <div class="card">
+            <div class="card-header">
+              <div class="row">
+                <div class="col">
+                  <div style="line-height: 34.6px;">ข้อมูลทุนการศึกษา - {{ data.name }}</div>
+                </div>
+                <div class="col text-right">
+                  <b-button variant="outline-primary" class="mt-1" size="sm">สมัครทุนการศึกษา</b-button>
+                </div>
+              </div>
+            </div>
+            <div class="card-body">
+              <div class="row">
+                <div class="col-12 col-md-6">
+                  <div class="image-container">
+                    <img v-if="data?.photo != null" :src="data.photo.path" :alt="data?.photo.name" />
+                  </div>
+                </div>
+                <div class="col-lg-6">
+                  <div class="row">
+                    <div class="col-12 col-md-4"><strong>หมวดหมู่ทุน</strong></div>
+                    <div class="col-12 col-md-8">ทุนให้เปล่า</div>
+                  </div>
+                  <div class="mb-1 row">
+                    <div class="col-12 col-md-4"><strong>ประเภททุนการศึกษา</strong></div>
+                    <div class="col-12 col-md-8">ทุนให้เปล่า</div>
+                  </div>
+                  <div class="mb-1 row">
+                    <div class="col-12 col-md-4"><strong>จำนวนเงิน</strong></div>
+                    <div class="col-12 col-md-8">{{ data.money?.toLocaleString('th-TH') }} บาท</div>
+                  </div>
+                  <div class="mb-1 row">
+                    <div class="col-12 col-md-4"><strong>จำนวนทุน</strong></div>
+                    <div class="col-12 col-md-8">{{ data.amount?.toLocaleString('th-TH') }} ทุน</div>
+                  </div>
+                  <div class="mb-1 row">
+                    <div class="col-12 col-md-4"><strong>การรับสมัคร</strong></div>
+                    <div class="col-12 col-md-8">
+                      <span class="badge badge-success" v-if="data.is_active">กำลังเปิดรับสมัคร</span>
+                      <span class="badge badge-warning" v-else>ปิดรับสมัคร</span>
+                    </div>
+                  </div>
+                  <div class="mb-1 row">
+                    <div class="col-12 col-md-4"><strong>ช่วงการรับสมัคร</strong></div>
+                    <div class="col-12 col-md-8">{{ onlyDate(data.from_date) }} - {{ onlyDate(data.to_date) }}</div>
+                  </div>
+                  <div class="mb-1 row">
+                    <div class="col-12 col-md-4"><strong>วันที่ประกาศผล</strong></div>
+                    <div class="col-12 col-md-8">{{ onlyDate(data.result_announcement_date) }}</div>
+                  </div>
+                  <div class="mb-1 row">
+                    <div class="col-12 col-md-4"><strong>ชั่วโมงกิจกรรม</strong></div>
+                    <div class="col-12 col-md-8">{{ data.activity_hours ?? '-' }}</div>
+                  </div>
+                  <div class="mb-1 row">
+                    <div class="col-12 col-md-4"><strong>เกรดเฉลี่ยขั้นต่ำ</strong></div>
+                    <div class="col-12 col-md-8">{{ data.minimum_grade ?? '-' }}</div>
+                  </div>
+                  <div class="mb-1 row">
+                    <div class="col-12 col-md-4"><strong>เอกสารประกอบ</strong></div>
+                    <div class="col-12 col-md-8">
+                      <a v-if="data.document" :href="data.document.path" target="_blank" noopener noreferrer>{{
+                        data.document.name }}</a>
+                      <span v-else>-</span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </b-col>
-      </b-row>
-      <b-row v-if="!loggedIn">
-        <b-col>
+      </div>
+      <div class="row mb-4">
+        <div class="col">
+          <div class="card">
+            <div class="card-header">รายละเอียดอื่น ๆ</div>
+            <div class="card-body">
+              <b-row v-if="data.content">
+                <b-col class="card-post-detail" v-html="data.content"></b-col>
+              </b-row>
+              <div v-else class="text-center">ไม่มีข้อมูลในส่วนนี้</div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="row" v-if="loggedIn">
+        <div class="col">
           <b-card header-tag="div" class="text-center">
             <div class="d-block">เข้าสู่ระบบเพื่อดำเนินการสมัครทุนการศึกษา</div>
-            <div class="d-block"><b-button variant="outline-success" size="sm" @click="$router.push('/login')">สมัครทุนการศึกษา</b-button></div>
+            <div class="d-block">
+              <b-button variant="outline-success" class="mt-1" size="sm" @click="$router.push('/login')">สมัครทุนการศึกษา</b-button>
+            </div>
           </b-card>
-        </b-col>
-      </b-row>
+        </div>
+      </div>
+      <div class="row" v-else>
+        <div class="col">
+          <div class="card">
+            <div class="card-body text-center">
+              <div class="d-block">
+                <b-button variant="outline-primary" class="mt-1" size="sm">สมัครทุนการศึกษา</b-button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </b-overlay>
   </b-container>
 </template>
@@ -54,6 +124,17 @@ export default {
     }
   },
   methods: {
+    onlyDate(datetime) {
+      return datetime ? new Date(datetime).toLocaleString('th-TH', {
+        year: 'numeric',
+        month: 'short',
+        day: 'numeric',
+        weekday: 'short'
+      }) : ''
+    },
+    onlyTime(datetime) {
+      return datetime
+    },
     formatdate(date) {
       return moment(date).add(543, 'years').format('DD/MM/YYYY HH:mm:ss')
     }
