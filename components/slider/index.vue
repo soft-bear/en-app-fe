075f536fbad706bc97d713d112d07d75e65bfd3f
subhibhat <s_subhibhat@hotmail.com>
2023-05-27 16:37:47 +0700

    update cover

--- components/slider/index.vue
+++ components/slider/index.vue
@@ -4,25 +4,26 @@
 			img-width="1024" img-height="280" style="text-shadow: 1px 1px 2px #333;" @sliding-start="onSlideStart"
 			@sliding-end="onSlideEnd">
 			<!-- Text slides with image -->
-			<b-carousel-slide caption="First slide" text="Nulla vitae elit libero, a pharetra augue mollis interdum."
-				img-src="https://picsum.photos/1024/180/?image=52"></b-carousel-slide>
+			<!-- <b-carousel-slide caption="First slide" text="Nulla vitae elit libero, a pharetra augue mollis interdum."
+				img-src="https://picsum.photos/1024/180/?image=52"></b-carousel-slide> -->
 
 			<!-- Slides with custom text -->
-			<b-carousel-slide img-src="https://picsum.photos/1024/180/?image=54">
+			<!-- <b-carousel-slide img-src="https://picsum.photos/1024/180/?image=54">
 				<h1>Hello world!</h1>
-			</b-carousel-slide>
+			</b-carousel-slide> -->
 
 			<!-- Slides with image only -->
-			<b-carousel-slide img-src="https://picsum.photos/1024/180/?image=58"></b-carousel-slide>
+			<b-carousel-slide img-src="/images/cover1.jpg"></b-carousel-slide>
+      <b-carousel-slide img-src="/images/cover2.jpg"></b-carousel-slide>
 
 			<!-- Slides with img slot -->
 			<!-- Note the classes .d-block and .img-fluid to prevent browser default image alignment -->
-			<b-carousel-slide>
+			<!-- <b-carousel-slide>
 				<template #img>
 					<img class="d-block img-fluid w-100" width="1024" height="180"
 						src="https://picsum.photos/1024/180/?image=55" alt="image slot">
 				</template>
-			</b-carousel-slide>
+			</b-carousel-slide> -->
 		</b-carousel>
 	</div>
 </template>
