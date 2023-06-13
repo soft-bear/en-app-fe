6b4f46f23d3b36dd91430761a3ae987c23278285
subhibhat <s_subhibhat@hotmail.com>
2023-06-13 22:00:07 +0700

    fix bug leave route

--- pages/scholarship/view/_id.vue
+++ pages/scholarship/view/_id.vue
@@ -7,33 +7,39 @@
 import { mapActions } from 'vuex'
 
 export default {
-	data() {
-		return {
-			loading: false,
+  data() {
+    return {
+      loading: false,
       scholarship: {},
       isShowForm: false
-		}
-	},
-	methods: {
+    }
+  },
+  methods: {
     ...mapActions({
       clearStep: 'application/clearStep'
     }),
-		async getScholarship() {
-			const {data : {data}} = await this.$axios.get(`/scholarships/${this.$route.params.id}`)
+    async getScholarship() {
+      const { data: { data } } = await this.$axios.get(`/scholarships/${this.$route.params.id}`)
       this.scholarship = data
-		},
+    },
     setIsShowForm(isShow) {
       this.isShowForm = isShow
     }
-	},
-	async created() {
-		this.loading = true
-		await this.getScholarship()
-		this.loading = false
-	},
+  },
+  async created() {
+    this.loading = true
+    await this.getScholarship()
+    this.loading = false
+  },
   beforeRouteLeave(to, from, next) {
-    if (this.isShowForm) {
-      this.$bvModal.msgBoxConfirm('คุณต้องการออกจากหน้านี้ใช่หรือไม่?', {
+    if (!this.isShowForm) {
+      this.clearStep();
+      next();
+      return;
+    }
+
+    this.$bvModal
+      .msgBoxConfirm('คุณต้องการออกจากหน้านี้ใช่หรือไม่?', {
         title: 'ยืนยันการออกจากหน้า',
         size: 'sm',
         buttonSize: 'sm',
@@ -42,17 +48,16 @@ export default {
         cancelTitle: 'ไม่ใช่',
         footerClass: 'p-2',
         hideHeaderClose: true,
-        centered: true
-      }).then(async value => {
-        if (value) {
-          this.clearStep()
-          next()
-        } else {
-          next(false)
-          return
-        }
+        centered: true,
       })
-    }
+      .then((value) => {
+        if (value) {
+          this.clearStep();
+          next();
+        } else {
+          next(false);
+        }
+      });
   }
 }
 </script>
